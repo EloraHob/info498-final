b5ee95376e00b39dd0440e4beb4a281a1deab84b
elorahob <elorahob@uw.edu>
2023-06-01 11:24:38 -0700

    test

---
+++ components/navbar.jsx
@@ -0,0 +1,13 @@
+import { Box, Link } from "@chakra-ui/react";
+import React from 'react';
+
+
+export default function Navbar() {
+    return (
+        <Box>
+            <Link href="/about">
+                ABOUT
+            </Link>
+        </Box>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import { Box, Heading, } from '@chakra-ui/react';
+
+export default function About() {
+  return (
+    <Box>
+      <Heading>
+        This is the heading for the about page!
+      </Heading>
+    </Box>
+  );
+}
