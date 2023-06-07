6fc5b222a6cba9e48b37fe73e8992216aa29e6c9
elorahob <elorahob@uw.edu>
2023-06-07 16:53:24 -0700

    oops

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
 }
 
 module.exports = {
   nextConfig,
-  assetPrefix: isProd ? '/info498-final' : '',
-  basePath: isProd ? '/info498-final' : '',
+  assetPrefix: '',
+  basePath: '',
   ignoreDuringBuilds: true,
 };
